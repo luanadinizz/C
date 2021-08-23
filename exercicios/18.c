ee0b96269f3ae51b43855057f87d50be03e8ac9c
luanadinizz <89155982+luanadinizz@users.noreply.github.com>
2021-08-23 17:21:58 -0300

    Create 18.c

---
+++ exercicios/18.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void main()
+{
+    float temp,vel,dist;
+    printf("insira a duracao da viagem em horas :\n");
+    scanf("%f",&temp);
+    printf("insira ")
+
+}
